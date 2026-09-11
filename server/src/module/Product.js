d498c1d466f0033b528d155cb4bbc69270bfe398
Chatchayapf <chatchaya.pf@gmai.com>
2026-09-11 11:43:59 +0700

    feat: initialize backend with Express and product CRUD operations
    - Added package.json for backend setup with Express dependency.
    - Created index.js to set up Express server and define CRUD routes for products.
    - Implemented product data management with create, read, update, and delete functionalities.
    - Added centralized error handling middleware.
    - Created Product.js module to store initial product data.
    - Added placeholder for future product routes in products.js.
    - Created user-api-test.rest file for API testing.

---
+++ server/src/module/Product.js
@@ -0,0 +1,32 @@
+export const product = [
+    {
+    id: "prod 1",
+    username: "BLINGY Album",
+    description: "The 7th Album - 2026",
+    price: "790 bath",
+    },
+    {
+    id: "prod 2",
+    username: "WALK",
+    description: "The 6th Full Album",
+    price: "590 bath",
+    },
+    {
+    id: "prod 3",
+    username: "Fact Check",
+    description: "The 5th Album",
+    price: "690 bath",
+    },
+    {
+    id: "prod 4",
+    username: "2 Baddies",
+    description: "The 4th Album",
+    price: "790 bath",
+    },
+    {
+    id: "prod 5",
+    username: "Neo Zone",
+    description: "The 2nd Album",
+    price: "690 bath",
+    },
+];
